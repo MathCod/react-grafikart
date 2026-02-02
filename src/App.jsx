0e5257ce24aa568fa0a8d093ddaa4c8967c610fe
Mathias BERGER <mathias.codage@gmail.com>
2026-02-02 19:08:12 +0100

    ➕ Premier Exos simple

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+const title = 'Bonjour les gens'
+const todos = [
+  'Apprendre le React',
+  'Apprendre le NodeJS',
+  'Apprendre le GraphQL'
+]
+
 
 function App() {
+
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const incrementCount = () => {
+    setCount(count + 1)
+  }
+  const decrementCount = () => {
+    setCount(count - 1)
+  }
+
+  const [person, setperson] = useState({
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 18
+  })
+
+  const incrementAge = () => {
+    setperson({...person, age: person.age + 1})
+  }
+  const decrementAge = () => {
+    setperson({...person, age: person.age - 1})
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    alert('Clic sur le titre effectué !')
+  }
+
+  const [value, setValue] = useState('')
+  const handleChange = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    setValue(e.target.value)
+  }
+
+  return  <>
+            <h1 onClick={handleClick} className="title">{title}</h1>
+            <ul>
+              {todos.map(todo => (<li key={todo}>{todo}</li>))}
+            </ul>
+            <div className='count'>
+              <p>Compteur : {count}</p>
+              <button onClick={incrementCount}>Incrémenter</button>
+              <button onClick={decrementCount}>Décrémenter</button>
+            </div>
+            <div className='age'>
+              <p>Age de {person.firstName} {person.lastName} : {person.age}</p>
+              <button onClick={incrementAge}>Gagner une anée</button>
+              <button onClick={decrementAge}>Perdre une anée</button>
+            </div>
+            <div>
+              <form>
+                <input type='text' name='firstname' value={value} onChange={handleChange} />
+                <button>Envoyer</button>
+              </form>
+            </div>
+          </>
 }
 
 export default App
